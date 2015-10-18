e7c9680a363f4a9630f6306fa29dc59c5755c009
Erick Arenas <earen003@fiu.edu>
2015-10-18 13:28:10 -0700

    pset complete

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 
+import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class Main
@@ -27,23 +31,102 @@ public class Main
         result = result.substring(1) + "#";
         System.out.println(result);
 
-       //test4 on the pset2 branch
+
     }
 
-    public static void main(String[] args) {
+    //creates mario pyramid with recursion//ouput to file
 
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter a number: ");
-
-        //loop for input, until it gets an int
-        while(!reader.hasNextInt())
+    public static void createpyramid(int a, PrintWriter writerx)
+    {
+        counter++;
+        if( a == 1)
         {
-            reader.next();
-            System.out.println("Enter a number: ");
+            counter = counter-1;
+            for(int i=0; i < counter; i++)
+            {
+                result = result + " ";
+            }
+            result = result + "##";
+            writerx.println(result);
+
+            return;
         }
 
-        int n = reader.nextInt();
+        createpyramid(a-1,writerx);
 
-        createpyramid(n);
+        result = result.substring(1) + "#";
+        writerx.println(result);
+
+    }
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("InputDialog");
+
+        // prompt the user to enter the number
+        String n = JOptionPane.showInputDialog(frame, "Please enter an integer?");
+        if(n == null)
+        {
+            System.out.println("You have canceled the program");
+            System.exit(0);
+        }
+        else
+        {
+            while (!n.matches("^?\\d+$")) {
+                n = JOptionPane.showInputDialog(frame, "Please enter an integer?");
+                if(n == null)
+                {
+                    System.out.println("You have canceled the program");
+                    System.exit(0);
+                    break;
+                }
+
+            }
+
+        }
+
+
+        Object[] options = {"Console",
+                "Text File"};
+        int m = JOptionPane.showOptionDialog(frame,
+                "Please select output method",
+                "Question",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,     //do not use a custom Icon
+                options,  //the titles of buttons
+                options[0]); //default button title
+
+
+
+        if(m == -1)
+        {
+            System.out.println("You have canceled the program");
+        }
+        if(m == 0)
+        {
+            createpyramid(Integer.parseInt(n));
+        }
+        if(m == 1)
+        {
+            try
+            {
+
+                PrintWriter writer = new PrintWriter("mario.txt", "UTF-8");
+                createpyramid(Integer.parseInt(n), writer);
+                writer.close();
+            } catch (FileNotFoundException e) {
+               System.out.println("File not found " + e.getMessage());
+            } catch (UnsupportedEncodingException e) {
+                System.out.println("Unsupported encoding " + e.getMessage());
+            }
+
+
+
+        }
+
+
+
+    System.exit(0);
     }
 }
